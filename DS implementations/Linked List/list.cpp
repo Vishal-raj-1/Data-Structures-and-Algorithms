5662a27adc268fa37fd632f4faa46d39940c9fab
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-10-10 22:50:39 +0530

    Adding Linked list

--- DS implementations/Linked List/list.cpp
+++ DS implementations/Linked List/list.cpp
@@ -14,9 +14,19 @@ struct Node
 
 //Pointer to head Node initialize to NULL
 Node *head = NULL;
+int size = 0;
 
 void insertAtNthNode(int x, int n)
 {
+    if (n < 1 || n > size + 1)
+    {
+        cout << "Position is not valid.\n";
+        return;
+    }
+
+    // Increase size
+    ++size;
+
     //Extra space for new Node
     Node *temp = (Node *)malloc(sizeof(Node));
 
@@ -43,13 +53,21 @@ void insertAtNthNode(int x, int n)
 
     //Store new address
     temp->next = temp1->next;
-
     temp1->next = temp;
 }
 
 //Fxn to delete N'th Node
 void Delete(int n)
 {
+    if (n < 1 || n > size + 1)
+    {
+        cout << "Position is not valid.\n";
+        return;
+    }
+
+    // Decrease the Size
+    --size;
+    
     Node *temp1 = head;
 
     if (n == 1)
@@ -66,11 +84,28 @@ void Delete(int n)
 
     //Pointer who point to N'th node
     Node *temp2 = temp1->next;
-
     temp1->next = temp2->next;
 
     //delete N'th node
     free(temp2);
+}
+
+void Update(int x, int n)
+{
+    if (n < 1 || n > size + 1)
+    {
+        cout << "Position is not valid.\n";
+        return;
+    }
+
+    Node* temp = head;
+    
+    // Iterate n-1 times
+    while (--n)
+        temp = temp->next;
+
+    //Update its value
+    temp->data = x;
 }
 
 //function to Reverse linked list
@@ -115,27 +150,34 @@ int main()
     {
         cout << "\n"
              << "Enter 1 for Insertion\n"
-             << "2 for Priniting List\n"
-             << "3 for Delete a node"
-             << "4 for Reverse given list"
-             << "5 for Quit\n";
+             << "2 for Delete a node\n"
+             << "3 for Updating Value\n"
+             << "4 for Priniting List\n"
+             << "5 for Reverse given list\n"
+             << "Any Number for Quit\n";
         cin >> op;
 
         if (op == 1)
         {
             cout << "Enter Number and its position : ";
-            cin >> n >> x;
-            insertAtNthNode(n, x);
+            cin >> x >> n;
+            insertAtNthNode(x, n);
         }
         else if (op == 2)
-            PrintList();
-        else if (op == 3)
         {
             cout << "Enter the position of Node : ";
             cin >> n;
             Delete(n);
         }
+        else if (op == 3)
+        {
+            cout << "Enter Number and its position : ";
+            cin >> x >> n;
+            Update(x, n);
+        }
         else if (op == 4)
+            PrintList();
+        else if (op == 5)
             Reverse();
         else
             break;
