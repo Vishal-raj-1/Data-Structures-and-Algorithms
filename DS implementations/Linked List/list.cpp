cac8873b40e88e75f06a0100cc5283b88e2eb10a
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-10-10 19:09:06 +0530

    Adding reverse function

--- DS implementations/Linked List/list.cpp
+++ DS implementations/Linked List/list.cpp
@@ -50,27 +50,45 @@ void insertAtNthNode(int x, int n)
 //Fxn to delete N'th Node
 void Delete(int n)
 {
-    Node* temp1 = head;
+    Node *temp1 = head;
 
-    if(n == 1)
+    if (n == 1)
     {
         //Head node is deleted and
         //Now head is pointed to next element
-        head = temp1 -> next;
+        head = temp1->next;
         free(temp1);
-        return ;
+        return;
     }
-    
-    for(int i=0;i<n-2;i++)
-      temp1 = temp1 -> next;
-    
+
+    for (int i = 0; i < n - 2; i++)
+        temp1 = temp1->next;
+
     //Pointer who point to N'th node
-    Node* temp2 = temp1 -> next;
-    
-    temp1 -> next = temp2 -> next;
-    
+    Node *temp2 = temp1->next;
+
+    temp1->next = temp2->next;
+
     //delete N'th node
     free(temp2);
+}
+
+//function to Reverse linked list
+void Reverse()
+{
+    Node *current, *prev, *next;
+
+    current = head;
+    prev = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
 }
 
 void PrintList()
@@ -96,10 +114,11 @@ int main()
     while (true)
     {
         cout << "\n"
-             << "Enter 1 for insertion\n"
+             << "Enter 1 for Insertion\n"
              << "2 for Priniting List\n"
              << "3 for Delete a node"
-             << "4 for Quit\n";
+             << "4 for Reverse given list"
+             << "5 for Quit\n";
         cin >> op;
 
         if (op == 1)
@@ -110,12 +129,14 @@ int main()
         }
         else if (op == 2)
             PrintList();
-        else if(op == 3)
+        else if (op == 3)
         {
             cout << "Enter the position of Node : ";
             cin >> n;
             Delete(n);
         }
+        else if (op == 4)
+            Reverse();
         else
             break;
     }
